7de3c2e0f508b20963e30f8a65979c8caa7515ca
Stav Shamir <shamir.stav@gmail.com>
2018-12-14 17:08:51 +0200

    Update AuthTokensController.java

--- src/main/java/com/stavshamir/app/authorization/AuthTokensController.java
+++ src/main/java/com/stavshamir/app/authorization/AuthTokensController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.Cookie;
@@ -36,16 +37,16 @@ public class AuthTokensController {
     }
 
     @RequestMapping("/callback")
-    public String callback(HttpServletResponse response, @RequestParam("code") String code) {
+    public ModelAndView callback(HttpServletResponse response, @RequestParam("code") String code) {
         try {
             String userId = authTokensService.retrieveAndPersistTokens(code);
             response.addCookie(new Cookie("spotify-user-uri", userId));
         } catch (IOException | SpotifyWebApiException e) {
-            return "Failed to retrieve authorization credentials from Spotify API: " + e.getMessage();
+//             return "Failed to retrieve authorization credentials from Spotify API: " + e.getMessage();
         }
 
         String redirectUrl = "https://stavshamir.github.io/slh-angular-client/";
-        return "redirect:" + redirectUrl;
+        return new ModelAndView("redirect:" + redirectUrl);
     }
 
 }
