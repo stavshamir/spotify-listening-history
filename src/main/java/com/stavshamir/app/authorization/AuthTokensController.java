dd4567fe0ed3f6d1c24950b6764c17d69a80eb3e
Stav Shamir <shamir.stav@gmail.com>
2018-12-16 09:28:54 +0200

    Update AuthTokensController.java

--- src/main/java/com/stavshamir/app/authorization/AuthTokensController.java
+++ src/main/java/com/stavshamir/app/authorization/AuthTokensController.java
@@ -2,9 +2,7 @@ package com.stavshamir.app.authorization;
 
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
