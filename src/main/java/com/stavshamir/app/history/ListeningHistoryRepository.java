86a5aed72b378a99b7e50b0f44c47b31854c5e1f
Stav Shamir <shamir.stav@gmail.com>
2018-12-17 14:36:32 +0200

    Update ListeningHistoryRepository.java

--- src/main/java/com/stavshamir/app/history/ListeningHistoryRepository.java
+++ src/main/java/com/stavshamir/app/history/ListeningHistoryRepository.java
@@ -20,8 +20,7 @@ public interface ListeningHistoryRepository extends PagingAndSortingRepository<L
                     " AND EXTRACT(MONTH from played_at) >= :from_month AND EXTRACT(MONTH from played_at) < :to_month" +
                     " AND EXTRACT(HOUR from played_at) >= :from_hour AND EXTRACT(HOUR from played_at) < :to_hour" +
                     " GROUP BY uri" +
-                    " ORDER BY COUNT(uri) DESC" +
-                    " LIMIT 100",
+                    " ORDER BY COUNT(uri) DESC"
             countQuery = "SELECT COUNT(*) FROM (" +
                     " SELECT COUNT(uri)" +
                     " FROM listening_history" +
